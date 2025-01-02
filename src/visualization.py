938ddebe91e0aca7de6f85917ae95771f3d601ce
Gifted <epopoola15@gmail.com>
2025-01-02 18:54:21 +0100

    Update customer segmenation model

--- src/visualization.py
+++ src/visualization.py
@@ -7,3 +7,24 @@ def plot_clusters(df, x_col, y_col, cluster_col):
     plt.ylabel(y_col)
     plt.title('Customer Segments')
     plt.show()
+
+# def evaluate_klusters():
+#     ks = range(1, 6)
+#     inertias = []
+
+#     for k in ks:
+#         # Create a KMeans instance with k clusters: model
+#         model = KMeans(n_clusters=k)
+        
+#         # Fit model to samples
+#         model.fit(samples)
+        
+#         # Append the inertia to the list of inertias
+#         inertias.append(model.inertia_)
+        
+#     # Plot ks vs inertias
+#     plt.plot(ks, inertias, '-o')
+#     plt.xlabel('number of clusters, k')
+#     plt.ylabel('inertia')
+#     plt.xticks(ks)
+#     plt.show()
