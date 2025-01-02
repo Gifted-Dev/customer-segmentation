05dd4a364dd08235eaa2ec19ac38c830be2e2f8f
Gifted <epopoola15@gmail.com>
2025-01-02 20:41:38 +0100

    update changes

--- src/visualization.py
+++ src/visualization.py
@@ -7,6 +7,28 @@ def plot_clusters(df, x_col, y_col, cluster_col):
     plt.ylabel(y_col)
     plt.title('Customer Segments')
     plt.show()
+    
+    
+# def plot_clusters_with_centroids(df, x_col, y_col, cluster_col, centroids):
+#     """Visualize clusters on a 2D scatter plot with centroids."""
+#     plt.figure(figsize=(8, 6))
+
+#     # Plot the clusters
+#     plt.scatter(df[x_col], df[y_col], c=df[cluster_col], cmap='viridis', label='Clusters', alpha=0.6)
+
+#     # Plot the centroids
+#     x_idx = centroid_features.index(x_col)
+    
+#     y_idx = centroid_features.index(y_col)
+#     plt.scatter(centroids[:, x_idx], centroids[:, y_idx], c='red', s=200, marker='X', label='Centroids')
+    
+
+#     plt.xlabel(x_col)
+#     plt.ylabel(y_col)
+#     plt.title('Customer Segments with Centroids')
+#     plt.legend()
+#     plt.show()
+   
 
 # def evaluate_klusters():
 #     ks = range(1, 6)
